d52f14b18326c2fae41f900d27925fcbb27b10d5
Ronell1243 <mesogem1243@gmail.com>
2026-06-15 15:31:55 +0900

    [fead] 인트로 컷씬 추가, 인트로 bgm 효과음 추가 교수님힘들어요

--- TopDown_Ronell/Assets/Script/CutSceneManager.cs
+++ TopDown_Ronell/Assets/Script/CutSceneManager.cs
@@ -10,6 +10,25 @@ public class CutSceneManager : MonoBehaviour
     [Header("컷씬 리스트")]
     public Sprite[] cutSceneSprites; // 여기에 사용할 그림들을 순서대로 드래그 앤 드롭
 
+    public AudioSource audioSource;
+
+    [Header("알람 소리")]
+    public AudioClip specialSound1; // 재생할 효과음
+    public int[] sound1TargetIndices;
+
+    [Header("냉장고 소리")]
+    public AudioClip specialSound2; // 재생할 효과음
+    public int[] sound2TargetIndices;
+
+    [Header("달리는 소리")]
+    public AudioClip specialSound3; // 재생할 효과음
+    public int[] sound3TargetIndices;
+    
+    [Header("반짝반짝 소리")]
+    public AudioClip specialSound4; // 재생할 효과음
+    public int[] sound4TargetIndices;
+
+
     [Header("다음으로 이동할 게임 씬")]
     public string nextSceneName;
 
@@ -49,6 +68,54 @@ public class CutSceneManager : MonoBehaviour
     void DisplayCutscene()
     {
         cutSceneDisplay.sprite = cutSceneSprites[currentIndex];
+
+        if (IsTargetIndex(currentIndex, sound1TargetIndices))
+        {
+            if (audioSource != null && specialSound1 != null)
+            {
+                audioSource.PlayOneShot(specialSound1);
+            }
+        }
+
+        if (IsTargetIndex(currentIndex, sound2TargetIndices))
+        {
+            if (audioSource != null && specialSound2 != null)
+            {
+                audioSource.PlayOneShot(specialSound2);
+            }
+        }
+
+        if (IsTargetIndex(currentIndex, sound3TargetIndices))
+        {
+            if (audioSource != null && specialSound3 != null)
+            {
+                audioSource.PlayOneShot(specialSound3);
+            }
+        }
+
+        if (IsTargetIndex(currentIndex, sound4TargetIndices))
+        {
+            if (audioSource != null && specialSound4 != null)
+            {
+                audioSource.PlayOneShot(specialSound4);
+            }
+        }
+    }
+
+    // 지정된 배열 안에 현재 컷씬 번호가 포함되어 있는지 확인하는 함수
+    bool IsTargetIndex(int index, int[] targetIndices)
+    {
+        if (targetIndices == null) return false;
+
+        // 배열을 돌면서 일치하는 숫자가 있는지 확인
+        for (int i = 0; i < targetIndices.Length; i++)
+        {
+            if (targetIndices[i] == index)
+            {
+                return true; // 일치하는 번호가 있으면 true 반환
+            }
+        }
+        return false; // 없으면 false 반환
     }
 
     void EndCutscene()
